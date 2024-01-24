7237d8f76222ef8a9a2a0eff2e8abfd1c3ef9a14
Matthijs Groen <matthijs.groen@gmail.com>
2024-01-24 22:53:25 +0100

    Only use components now

--- packages/app/src/ExperimentScreen.events.ts
+++ packages/app/src/ExperimentScreen.events.ts
@@ -3,7 +3,7 @@ import type { Scope } from "./ExperimentScreen.rui";
 export default (scope: Scope) => ({
   demoButtonClick: (event: React.MouseEvent): void => {
     console.log("Woohoo", scope.user.value);
-    scope.user.value = "World";
+    scope.text1.content = "UpdatedContent!";
   },
   button2Click: (event: React.MouseEvent): void => {
     console.log("hello!");
