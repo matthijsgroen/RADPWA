016ee548b7bddcb046c11c843ad5b91db9a7437f
Roy <roy.appeldoorn@gmail.com>
2024-01-24 13:31:27 +0100

    Copy multiple files

--- packages/app/scripts/copy-to-extension.js
+++ packages/app/scripts/copy-to-extension.js
@@ -1,16 +1,19 @@
-const fs = require("fs");
+const fs = require("fs-extra");
 const path = require("path");
 
-const sourcePath = path.join(__dirname, "../dist/index.1f77fd16.js");
-const destPath = path.join(
-  __dirname,
-  "../../vscode-rui-editor/media/index.1f77fd16.js",
-);
+const sourceDir = path.join(__dirname, "../dist");
+const destDir = path.join(__dirname, "../../vscode-rui-editor/media");
 
-fs.copyFile(sourcePath, destPath, (err) => {
-  if (err) {
-    console.error("Error copying file:", err);
-    return;
-  }
-  console.log("File copied successfully.");
+const filesToCopy = ["index.1f77fd16.js", "index.d82e8cfa.css"];
+
+filesToCopy.forEach((file) => {
+  const sourceFile = path.join(sourceDir, file);
+  const destFile = path.join(destDir, file);
+
+  fs.copy(sourceFile, destFile, function (err) {
+    if (err) {
+      return console.error(`Error copying ${file}:`, err);
+    }
+    console.log(`Copied ${file} to extension media directory.`);
+  });
 });
