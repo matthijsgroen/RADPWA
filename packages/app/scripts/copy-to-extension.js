77bf42c52fed4b4e72b20bb5582192ad63baecae
Roy <roy.appeldoorn@gmail.com>
2024-01-25 10:07:58 +0100

    Copy multiple files after build

--- packages/app/scripts/copy-to-extension.js
+++ packages/app/scripts/copy-to-extension.js
@@ -4,7 +4,12 @@ const path = require("path");
 const sourceDir = path.join(__dirname, "../dist");
 const destDir = path.join(__dirname, "../../vscode-rui-editor/media");
 
-const filesToCopy = ["index.1f77fd16.js", "index.d82e8cfa.css"];
+const filesToCopy = [
+  "index.048cd1c8.js",
+  "index.59a8f4de.css",
+  "Inter-italic.var.6a2c71c9.woff2",
+  "Inter-roman.var.40521a19.woff2",
+];
 
 filesToCopy.forEach((file) => {
   const sourceFile = path.join(sourceDir, file);
