1e3d09139ac1612c4188ecf70e1c9ce24064ad11
Roy <roy.appeldoorn@gmail.com>
2024-02-07 16:05:25 +0100

    Add primeicons

--- packages/editor/src/App.tsx
+++ packages/editor/src/App.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ManualScreen from "./ManualScreen";
+import "primeicons/primeicons.css";
 import { PrimeReactProvider } from "primereact/api";
 import "primereact/resources/themes/lara-light-purple/theme.css";
 
