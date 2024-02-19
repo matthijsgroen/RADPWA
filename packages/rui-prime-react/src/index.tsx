f158bdfcaca9190963eb5424e919254282739607
Roy <roy.appeldoorn@gmail.com>
2024-02-19 14:57:38 +0100

    Add form inputs

--- packages/rui-prime-react/src/index.tsx
+++ packages/rui-prime-react/src/index.tsx
@@ -1,10 +1,22 @@
-import { Panel as PrimeReactPanel } from "primereact/panel";
-import { Button as PrimeReactButton } from "primereact/button";
 import React from "react";
+
 import type {
   VisualComponentDefinition,
   ComponentLibrary,
 } from "@rui/transform";
+
+import { Panel as PrimeReactPanel } from "primereact/panel";
+import { Button as PrimeReactButton } from "primereact/button";
+import {
+  RadioButton as PrimeReactRadioButton,
+  RadioButtonChangeEvent,
+} from "primereact/radiobutton";
+import {
+  ListBoxChangeEvent,
+  ListBox as PrimeReactListbox,
+} from "primereact/listbox";
+import { InputText as PrimeReactInputText } from "primereact/inputtext";
+import { SelectItemOptionsType } from "primereact/selectitem";
 
 const Panel: VisualComponentDefinition<
   { header: string },
@@ -30,9 +42,80 @@ const Button: VisualComponentDefinition<
   ),
 };
 
+const RadioButton: VisualComponentDefinition<
+  {
+    inputId: string;
+    name: string;
+    value: string;
+    disabled: boolean;
+    checked: boolean;
+  },
+  { onChange: (event: RadioButtonChangeEvent) => void },
+  {}
+> = {
+  vc: (props) => (
+    <PrimeReactRadioButton
+      inputId={props.inputId}
+      name={props.name}
+      value={props.value}
+      disabled={props.disabled}
+      checked={props.checked}
+      onChange={props.onChange}
+    />
+  ),
+};
+
+const Listbox: VisualComponentDefinition<
+  {
+    name: string;
+    value: string;
+    options: SelectItemOptionsType;
+    optionLabel: string;
+  },
+  { onChange: (event: ListBoxChangeEvent) => void },
+  {}
+> = {
+  vc: (props) => (
+    <PrimeReactListbox
+      name={props.name}
+      value={props.value}
+      options={props.options}
+      optionLabel={props.optionLabel}
+      onChange={props.onChange}
+    />
+  ),
+};
+
+const InputText: VisualComponentDefinition<
+  {
+    name: string;
+    value: string;
+    disabled: boolean;
+  },
+  {
+    onInput: (
+      event: React.FormEvent<HTMLInputElement>,
+      validatePattern: boolean,
+    ) => void;
+  },
+  {}
+> = {
+  vc: (props) => (
+    <PrimeReactInputText
+      name={props.name}
+      value={props.value}
+      disabled={props.disabled}
+      onInput={props.onInput}
+    />
+  ),
+};
+
 const componentLibrary = {
   Panel,
   Button,
+  RadioButton,
+  Listbox,
+  InputText,
 } as const satisfies ComponentLibrary;
 
 export default componentLibrary;
