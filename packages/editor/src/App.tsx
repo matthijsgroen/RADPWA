7e9ef45a1b7e68852f7220bb02626a511bca482e
Matthijs Groen <matthijs.groen@gmail.com>
2024-02-24 13:58:32 +0100

    Reduce whitespace

--- packages/editor/src/App.tsx
+++ packages/editor/src/App.tsx
@@ -1,12 +1,37 @@
 import React from "react";
 import ManualScreen from "./ManualScreen";
 import "primeicons/primeicons.css";
-import { PrimeReactProvider } from "primereact/api";
+import { APIOptions, PrimeReactProvider } from "primereact/api";
 import "primereact/resources/themes/lara-light-purple/theme.css";
 
 export const App = () => {
+  const value: APIOptions = {
+    pt: {
+      panel: {
+        header: { className: "p-4" },
+      },
+      tree: {
+        root: { className: "p-1" },
+        node: {
+          className: "p-0",
+        },
+        content: {
+          className: "p-0",
+        },
+        subgroup: {
+          className: "p-l-0",
+        },
+      },
+      tabview: {
+        panelContainer: { className: "p-0 pt-2" },
+      },
+      tabpanel: {
+        headerAction: { className: "p-2" },
+      },
+    },
+  };
   return (
-    <PrimeReactProvider>
+    <PrimeReactProvider value={value}>
       <ManualScreen />
     </PrimeReactProvider>
   );
